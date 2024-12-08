34c57e5b7bec43897a4eb160652ccd7654ced510
Erio-Harrison <u7541840@gmail.com>
2024-12-08 11:24:01 +1100

    translate addresses using OffsetPageTable

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,18 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use rust_os::println;
+use rust_os::{memory::translate_addr, println};
+use bootloader::BootInfo;
+use x86_64::{structures::paging::PageTable, VirtAddr};
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start(boot_info : &'static BootInfo) -> ! {
+    use rust_os::memory;
+    use x86_64::{structures::paging::Translate, VirtAddr};
     println!("Hello World{}", "!");
     rust_os::init();
-    use x86_64::registers::control::Cr3;
+    
 
-    let (level_4_page_table, _) = Cr3::read();
-    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
     // println!("Timer interrupt test starting...");
     
     // loop {
@@ -40,6 +42,40 @@ pub extern "C" fn _start() -> ! {
     // // write to a code page
     // unsafe { *ptr = 42; }
     // println!("write worked");
+
+    // use rust_os::memory::active_level_4_table;
+    // use x86_64::VirtAddr;
+
+    // let (level_4_page_table, _) = Cr3::read();
+    // println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
+
+    // for (i, entry) in l4_table.iter().enumerate() {
+    //     if !entry.is_unused() {
+    //         println!("L4 Entry {}: {:?}", i, entry);
+    //     }
+    // }
+
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
+    }
 
     #[cfg(test)]
     test_main();
